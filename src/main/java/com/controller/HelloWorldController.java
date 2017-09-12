de7485e5831a1464276da4bf994bc27cd3be5dd8
dengqunhua <dengqunhua_123@163.com>
2017-09-12 15:23:27 +0800

    增加模版配置

--- src/main/java/com/controller/HelloWorldController.java
+++ src/main/java/com/controller/HelloWorldController.java
@@ -2,18 +2,23 @@ package com.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.domain.UserProperties;
-
-@RestController
+@Controller
 public class HelloWorldController {
 	@Autowired
 	private UserProperties userProperties;
 	
 	@RequestMapping("/hello")
+	@ResponseBody
 	public String hello(){
 		return "hello "+userProperties.getName();
+	}
+	
+	@RequestMapping(value={"/template","/tpl"})
+	public String template(){
+		return "index";
 	}
 }
--- src/main/java/com/controller/UserProperties.java
+++ src/main/java/com/controller/UserProperties.java
@@ -1,4 +1,4 @@
-package com.domain;
+package com.controller;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
